5f8df35a8864b9b1815cbebdceec5c72c120c51a
tomryus <sr.tomqq@gmail.com>
2019-10-14 06:25:11 +0700

    inisiasi awal

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,35 @@ class HomePage  extends StatelessWidget
               leading: Icon(Icons.home),
               title: Text('Aplikasi Basic')
               ),
+
+
+              body: Container(
+                margin: EdgeInsets.all(10.0),
+                
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                        Icon(Icons.file_download),
+                        Text('klik tombol dibawah'),
+                        ]
+                      ),
+                      
+                     Card(
+                       child:Column(
+                         children: <Widget>[
+                           Image.network('https://flutter.io/images/homepage/header-illustration.png'),
+                          Text('Belajar Flutter \n hahahaha')
+                         ],
+                       ),
+                     )
+                      
+                    
+                      
+                    ],
+                  )
+                )
+              
             )
         );
     }
